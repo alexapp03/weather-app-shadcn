29d6efdd4dc93319570dc85e276fc8e8aba3655b
Alexandr Basiuk <basok.sanya@gmail.com>
2025-05-14 00:22:48 +0300

    add header

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import ThemeToggle from '@/components/ThemeToggle.vue'
+</script>
+
+<template>
+  <header class="container mx-auto">
+    <div class="flex items-center justify-between w-full py-4">
+      <div class="w-20"></div>
+
+      <nav class="flex items-center space-x-4">
+        <RouterLink to="/" class="hover:text-primary">Home</RouterLink>
+      </nav>
+
+      <div class="w-20 flex justify-end">
+        <ThemeToggle />
+      </div>
+    </div>
+  </header>
+</template>
